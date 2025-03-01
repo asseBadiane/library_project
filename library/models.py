d698e01992097d31856ca3b6638fc300e17e52f7
asseBadiane <badianeasse99@gmail.com>
2025-03-01 15:11:45 +0000

    🎨 Create the models and do their migrations

--- library/models.py
+++ library/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13, unique=True)
+    description = models.TextField(blank=True)
+    publication_date = models.DateField(null=True, blank=True)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
+    number_of_copies = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+class Loan(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)  # Lien vers l'app users
+    loan_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    return_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    ADMIN = 'ADMIN'
+    LIBRARIAN = 'LIBRARIAN'
+    READER = 'READER'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (LIBRARIAN, 'Librarian'),
+        (READER, 'Reader'),
+    ]
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=READER,
+    )
+
+    def __str__(self):
+        return self.username

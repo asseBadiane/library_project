9215dbc44dcf0892639a86b4360a9c8a0d4e6860
asseBadiane <badianeasse99@gmail.com>
2025-03-02 16:32:49 +0000

    🔒️ Add and config CORS

--- library/models.py
+++ library/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
 class Author(models.Model):
     name = models.CharField(max_length=200)
     birth_date = models.DateField(null=True, blank=True)
--- library_project/settings.py
+++ library_project/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # Ajouter rest_framework
+    "corsheaders",
     'library',
     'users'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  # Ajouter CorsMiddleware 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
 ]
 
 ROOT_URLCONF = 'library_project.urls'
